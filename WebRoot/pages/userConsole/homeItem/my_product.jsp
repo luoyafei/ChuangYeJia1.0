cd0352858d12cd3a92f1becf77b259f6492e6f77
luoyafei <luoyafei.newinstance@foxmail.com>
2016-08-28 21:23:14 +0800

    add myProduct application ok

--- WebRoot/pages/userConsole/homeItem/my_product.jsp
+++ WebRoot/pages/userConsole/homeItem/my_product.jsp
@@ -1,12 +1,61 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>我的产品</title>
-</head>
-<body>
+    
 
-</body>
-</html>
+	
+
+<div role="tabpanel" class="tab-pane" id="messages">
+	<div style="width: 100%;">
+		<div class="" style="width: 100%;height: 100px;">
+			<div style="overflow-y: auto;" id="productField">
+				<a class="uk-thumbnail productHref">
+					<img src="" alt="" class="productConver" style="width: 200px;height: 100px;">
+						<div class="uk-thumbnail-caption productName" style="width: 200px;white-space: nowrap;overflow: hidden;">
+						尚未创建产品
+						</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+	
+	
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		$.post('getProductsInConsoleForUser!getProductsInConsoleForUser.action', {
+			fromConsole : true
+		}, function(data, textStatus) {
+			if(textStatus == 'success') {
+				
+				if(data.success) {
+					for(var i = 0; i < data.ps.length-1; i++) {
+						$("#productField").append($(".productHref").clone().attr("class", "uk-thumbnail productFieldClone"));
+					}
+					for(var i = 0;i < data.ps.length; i++) {
+						
+						if(i === 0) {
+							$(".productHref").eq(i).attr("href", "getProductItem?item="+data.ps[i].productId);
+							$(".productName").eq(i).text(data.ps[i].productName);
+							$(".productConver").eq(i).attr("src", data.ps[i].productCover);
+						} else {
+							$(".productFieldClone").eq(i-1).attr("href", "getProductItem?item="+data.ps[i].productId);
+							$(".productName").eq(i).text(data.ps[i].productName);
+							$(".productConver").eq(i).attr("src", data.ps[i].productCover);
+						}
+						
+					}
+				} else {
+					alert("获取产品出错，请刷新重试");
+				}
+			} else {
+				alert("网络出错！请刷新重试！");
+			}
+		}, 'json');
+	});
+</script>
+	
+
+
+
+
--- src/com/chuangyejia/ajax/GetProductsInStartupsIdAction.java
+++ src/com/chuangyejia/ajax/GetProductsInStartupsIdAction.java
@@ -2,6 +2,7 @@ package com.chuangyejia.ajax;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -12,7 +13,10 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.chuangyejia.bean.Product;
+import com.chuangyejia.bean.Startups;
+import com.chuangyejia.bean.User;
 import com.chuangyejia.service.IProductService;
+import com.chuangyejia.service.IStartupsService;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.opensymphony.xwork2.ActionSupport;
@@ -27,6 +31,7 @@ public class GetProductsInStartupsIdAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
 
 	private IProductService ps;
+	private IStartupsService ss;
 	public IProductService getPs() {
 		return ps;
 	}
@@ -35,23 +40,34 @@ public class GetProductsInStartupsIdAction extends ActionSupport {
 		this.ps = ps;
 	}
 	
+	public IStartupsService getSs() {
+		return ss;
+	}
+	@Resource(name="ss")
+	public void setSs(IStartupsService ss) {
+		this.ss = ss;
+	}
 	
-	private String startupsId;
 	
+	private String startupsId = null;//获取从展示Startups的页面的请求
+	private boolean fromConsole = false;//用来获取从控制台来的请求
 	
 	public String getStartupsId() {
 		return startupsId;
 	}
-
-
 	public void setStartupsId(String startupsId) {
 		this.startupsId = startupsId;
+	}
+	
+	public boolean isFromConsole() {
+		return fromConsole;
+	}
+	public void setFromConsole(boolean fromConsole) {
+		this.fromConsole = fromConsole;
 	}
 
 
 	public void getProductsInStartups() {
-		
-		
 		HttpServletResponse response = ServletActionContext.getResponse();
 		response.setContentType("application/json; charset=utf-8");
 		PrintWriter out = null;
@@ -65,7 +81,7 @@ System.out.println("GetProductInStartupsIdAction中获取输出管道出错！")
 		
 		boolean success = false;
 		
-		if(startupsId != null && startupsId.trim().hashCode() != 0) {
+		if(startupsId != null && startupsId.trim().hashCode() != 0 && !fromConsole) {
 			List<Product> prs = ps.getProductsInStartupsId(startupsId);
 			for(int i = 0; i < prs.size(); i++)
 				prs.get(i).setProductStartups(null);
@@ -79,5 +95,46 @@ System.out.println("GetProductInStartupsIdAction中获取输出管道出错！")
 		out.flush();
 		out.close();
 		
+	}
+	
+	public void getProductsInConsoleForUser() {
+		HttpServletResponse response = ServletActionContext.getResponse();
+		response.setContentType("application/json; charset=utf-8");
+		PrintWriter out = null;
+		User user = (User)ServletActionContext.getRequest().getSession().getAttribute("user");
+		try {
+			out = response.getWriter();
+		} catch(IOException e) {
+System.out.println("GetProductInStartupsIdAction中获取输出管道出错！");
+		}
+		Gson gson = new Gson();
+		JsonObject jo = new JsonObject();
+		
+		boolean success = false;
+		if(user != null && startupsId == null && fromConsole) {
+			
+			List<Startups> startups = ss.getStartupsInLeaderId(user.getUserId());
+			List<Product> psList = new ArrayList<Product>();
+			if(startups != null && startups.size() > 0) {
+				for(int i = 0; i < startups.size(); i++) {
+					List<Product> prs = ps.getProductsInStartupsId(startups.get(i).getStartupsId());
+					if(prs != null && prs.size() > 0)
+						psList.addAll(prs);
+				}
+				for(int i = 0; i < psList.size(); i++)
+					psList.get(i).setProductStartups(null);
+				jo.add("ps", gson.toJsonTree(psList));
+				success = true;
+				
+			}
+			
+			
+		}
+		
+		jo.addProperty("success", success);
+		out.print(jo.toString());
+		
+		out.flush();
+		out.close();
 	}
 }
