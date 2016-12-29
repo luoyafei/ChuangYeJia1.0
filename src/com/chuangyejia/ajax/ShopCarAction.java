640219dcd1fbd042c44a19cc4de64636f4d3304c
luoyafei <luoyafei.newinstance@foxmail.com>
2016-12-29 14:15:07 +0800

    增加移除商品从购物车中

--- src/com/chuangyejia/ajax/ShopCarAction.java
+++ src/com/chuangyejia/ajax/ShopCarAction.java
@@ -2,6 +2,7 @@ package com.chuangyejia.ajax;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 import java.util.Vector;
 
 import javax.annotation.Resource;
@@ -25,11 +26,18 @@ import com.opensymphony.xwork2.ActionSupport;
 public class ShopCarAction extends ActionSupport {
 
 	private String productId;
+	private String deleteProductId;//传递过来的是json数据，进行解析成string[]
 	public String getProductId() {
 		return productId;
 	}
 	public void setProductId(String productId) {
 		this.productId = productId;
+	}
+	public String getDeleteProductId() {
+		return deleteProductId;
+	}
+	public void setDeleteProductId(String deleteProductId) {
+		this.deleteProductId = deleteProductId;
 	}
 	
 	private IShopCarService iscs;
@@ -124,5 +132,86 @@ public class ShopCarAction extends ActionSupport {
 		out.print(jo.toString());
 		out.flush();
 		out.close();
+	}
+	
+	/**
+	 * 用来转换从前台传输过来的ids
+	 * @author Diamond
+	 *
+	 */
+	private static class P {
+		private String[] ids;
+		
+		public String[] getIds() {
+			return ids;
+		}
+		public void setIds(String[] ids) {
+			this.ids = ids;
+		}
+		
+		public boolean isRrightFormat() {
+			for(int i = 0; i < ids.length; i++) {
+				if(ids[i].length() != 32)
+					return false;
+			}
+			return true;
+		}
+	}
+	/**
+	 * 从购物车中删除产品
+	 */
+	public void deleteItem() {
+		
+		HttpServletResponse response = ServletActionContext.getResponse();
+		response.setContentType("application/json; charset=utf-8");
+		PrintWriter out = null;
+		try {
+			out = response.getWriter();
+		} catch(IOException e) {}
+		
+		JsonObject jo = new JsonObject();
+		boolean success = true;
+		String reason = "";
+		User user = (User)ServletActionContext.getRequest().getSession().getAttribute("user");
+		if(user != null) {
+			Gson gson = new Gson();
+			P pIds = gson.fromJson(deleteProductId, P.class);
+			if(pIds.isRrightFormat()) {
+				@SuppressWarnings("unchecked")
+				Vector<Product> shopCar = (Vector<Product>)ServletActionContext.getRequest().getSession().getAttribute("shopCar");
+				
+				if(iscs.deleteProductInShopCar(user.getUserId(), pIds.getIds())) {
+					Iterator<Product> iterP = shopCar.iterator();
+					for(String pId : pIds.getIds()) {
+						while(iterP.hasNext()) {
+							if(iterP.next().getProductId().equals(pId)) {
+								iterP.remove();
+								break;
+							}
+						}
+					}
+					ServletActionContext.getRequest().getSession().setAttribute("shopCar", shopCar);
+				} else {
+					success = false;
+					reason = "删除商品失败！";
+				}
+			} else {
+				success = false;
+				reason = "产品出错";
+			}
+			
+			
+			
+		} else {
+			success = false;
+			reason = "尚未登陆！";
+		}
+		jo.addProperty("success", success);
+		jo.addProperty("reason", reason);
+		
+		out.print(jo.toString());
+		out.flush();
+		out.close();
+		
 	}
 }
